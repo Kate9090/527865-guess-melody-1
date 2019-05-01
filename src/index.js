ab77d9c612c1f6e644753de605e8b0ab93c73c83
eremina-ekaterina <eremina@uchi.ru>
2019-05-01 17:54:10 +0300

    add files for first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+import Welcome from './components/welcome/welcome';
+
+const App = () => {
+  // const {gameTime, errorCount} = props;
+
+  return <Welcome
+    // time={gameTime}
+    // errorCount={errorCount}
+  />;
+};
+
+const init = () => {
+  const settings = {
+    gameTime: 5,
+    errorCount: 3,
+  };
+  ReactDOM.render(
+      <App
+        errorCount={settings.errorCount}
+        gameTime={settings.gameTime
+        }/>, document.querySelector(`.main`));
+};
+
+init();
